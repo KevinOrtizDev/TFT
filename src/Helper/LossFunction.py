080f51637abe34a8ceb4c7edd2975c19d78dfde4
KevinOrtizDev <ortiz0kevin0falcon@gmail.com>
2023-05-14 12:30:59 +0100

    Se elimina imports que no se están utilizando y se actualiza la prueba realizada de CNN_FOCALLOSS

--- src/Helper/LossFunction.py
+++ src/Helper/LossFunction.py
@@ -22,8 +22,9 @@ def iou(pred, target, smooth=1e-4):
     target = target.contiguous() 
     intersection = (pred * target).sum(dim=2).sum(dim=2)
     union = pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2)
-    union[union==-smooth]=smooth
-    return ((intersection + smooth) / (union + smooth)).mean()
+    union=union+smooth
+    union[union==0]=smooth
+    return ((intersection + smooth) / union).mean()
     
 
 def iou_loss(pred, target, smooth = 1e-4):
@@ -40,8 +41,6 @@ def iou_loss(pred, target, smooth = 1e-4):
 def tversky_index(pred, target, alpha , beta,smooth = 1e-4):
     pred = pred.contiguous()
     target = target.contiguous()    
-    print(alpha)
-    print(beta)
     intersection = (pred * target).sum(dim=2).sum(dim=2)
     union = pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2)
 
